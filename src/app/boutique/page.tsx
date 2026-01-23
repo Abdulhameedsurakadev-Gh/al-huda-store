3108681ed9dd0cb34b13bf3e89c67bbd9a934e20
Abdul Hameed Suraka <abdulhameedsuraka.dev@gmail.com>
2026-01-22 21:29:54 -0800

    fix:added default export to boutique page

---
+++ src/app/boutique/page.tsx
@@ -0,0 +1,10 @@
+// src/app/boutique/page.tsx
+
+export default function BoutiquePage() {
+  return (
+    <div>
+      <h1>Welcome to the Al-Huda Boutique</h1>
+      {/* Your items will go here */}
+    </div>
+  );
+}
