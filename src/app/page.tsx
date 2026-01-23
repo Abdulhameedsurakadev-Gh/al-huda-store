d35c2c2162d8104fe22ccd097c395b70ebe49280
Abdul Hameed Suraka <abdulhameedsuraka.dev@gmail.com>
2026-01-23 15:23:45 -0800

    fix: added small styling to the contact page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import CategoryPulse from "@/components/CategoryPulse";
+import ContactGuide from "@/components/ContactGuide";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <main >
           <Hero />
           <CategoryPulse />
+          <ContactGuide />
       </main>
     </div>
   );
