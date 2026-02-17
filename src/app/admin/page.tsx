c7768174e6b733ac106bf8b2c134fc109e920f49
Abdul Hameed Suraka <abdulhameedsuraka.dev@gmail.com>
2026-02-17 07:11:46 -0800

    fix: testing whether the admin page is working

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+export default function AdminLogin() {
+    return (
+        <div className="min-h-screen bg-greeen-700 flex items-center justify-center">
+            <div className="bg-white p-8 rounded-xl shadow-xl">
+                <h1 className="text-3xl font-bold text-gray-800>AL-HUDA Admin</h1>
+                <p className="text-gray-800 mt-2>Admin panel iis working!</h1>
+                </div>
+        </div>
+    );
+
+}
